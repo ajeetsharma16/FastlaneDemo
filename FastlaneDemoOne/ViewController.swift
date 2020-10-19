1061f0ea2b67e638f598e97c7cc66bfe00011c89
ajeetsharma16 <asharma@webcubator.co>
2020-10-19 23:47:22 +0530

    NEW PROJECT

---
+++ FastlaneDemoOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastlaneDemoOne
+//
+//  Created by Webcubator Technologies LLP on 19/10/20.
+//  Copyright © 2020 Webcubator Technologies LLP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
